2407233df85938ddc1c98d0d5a0c6385e323cfc1
ElizavetaBelskya <90205133+ElizavetaBelskya@users.noreply.github.com>
2023-07-03 11:39:19 +0300

    Order updated

--- src/main/java/ru/tinkoff/storePrime/models/Order.java
+++ src/main/java/ru/tinkoff/storePrime/models/Order.java
@@ -8,12 +8,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.tinkoff.storePrime.models.user.Customer;
 
+import java.util.List;
+import java.util.Map;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 @Entity
-@Table(name = "cart_item")
+@Table(name = "market_order")
 public class Order {
 
     public enum Status {
@@ -24,10 +27,22 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
-    private Product product;
+    @ManyToMany
+    @JoinTable(
+            name = "order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
 
-    private Integer amount;
+    @ElementCollection
+    @CollectionTable(
+            name = "order_product_amount",
+            joinColumns = @JoinColumn(name = "order_id")
+    )
+    @MapKeyJoinColumn(name = "product_id")
+    @Column(name = "amount")
+    private Map<Product, Integer> productAmounts;
 
     private Status status;
 
