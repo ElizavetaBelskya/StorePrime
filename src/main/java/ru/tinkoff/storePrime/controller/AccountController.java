197b977b6b9dd45c64d286a4358c32ff521428a3
ElizavetaBelskya <90205133+ElizavetaBelskya@users.noreply.github.com>
2023-07-08 14:54:37 +0300

    AccountController added

---
+++ src/main/java/ru/tinkoff/storePrime/controller/AccountController.java
@@ -0,0 +1,31 @@
+package ru.tinkoff.storePrime.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.tinkoff.storePrime.controller.api.AccountApi;
+import ru.tinkoff.storePrime.dto.user.AccountDto;
+import ru.tinkoff.storePrime.dto.user.CustomerDto;
+import ru.tinkoff.storePrime.dto.user.SellerDto;
+import ru.tinkoff.storePrime.exceptions.ForbiddenException;
+import ru.tinkoff.storePrime.models.user.Account;
+import ru.tinkoff.storePrime.models.user.Customer;
+import ru.tinkoff.storePrime.models.user.Seller;
+import ru.tinkoff.storePrime.security.details.UserDetailsImpl;
+
+@RestController
+public class AccountController implements AccountApi {
+
+    @Override
+    public ResponseEntity<AccountDto> getThisAccount(UserDetailsImpl userDetailsImpl) {
+        Account account = userDetailsImpl.getAccount();
+        if (Account.Role.CUSTOMER.equals(account.getRole())) {
+            return ResponseEntity.ok(CustomerDto.from((Customer) account));
+        } else if (Account.Role.SELLER.equals(account.getRole())) {
+            return ResponseEntity.ok(SellerDto.from((Seller) account));
+        } else {
+            throw new ForbiddenException("Запрет доступа");
+        }
+    }
+
+
+}
---
+++ src/main/java/ru/tinkoff/storePrime/controller/api/AccountApi.java
@@ -0,0 +1,51 @@
+package ru.tinkoff.storePrime.controller.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.tinkoff.storePrime.dto.exception.ExceptionDto;
+import ru.tinkoff.storePrime.dto.user.AccountDto;
+import ru.tinkoff.storePrime.dto.user.CustomerDto;
+import ru.tinkoff.storePrime.dto.user.SellerDto;
+import ru.tinkoff.storePrime.security.details.UserDetailsImpl;
+
+public interface AccountApi {
+
+
+    @Operation(summary = "Получение аккаунта пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Информация об аккаунте покупателя",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CustomerDto.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "200", description = "Информация об аккаунте продавца",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = SellerDto.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "404", description = "Сведения об ошибке: пользователь не найден",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionDto.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "403", description = "Сведения об ошибке: доступ запрещен",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ExceptionDto.class))
+                    }
+            )
+    })
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('CUSTOMER', 'SELLER')")
+    ResponseEntity<AccountDto> getThisAccount(UserDetailsImpl userDetailsImpl);
+
+}
